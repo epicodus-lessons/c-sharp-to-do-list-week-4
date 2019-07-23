6a71c0a998500dc9ed1469820a8ab653fe666077
Elly Maimon <eliranmaimon@gmail.com>
2019-07-23 11:46:20 -0700

    Retrieving Objects from the Database

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using MySql.Data.MySqlClient;
 
 namespace ToDoList.Models
 {
@@ -6,28 +7,49 @@ namespace ToDoList.Models
   {
     public string Description { get; set; }
     public int Id { get; }
-    private static List<Item> _instances = new List<Item> { };
 
     public Item(string description)
     {
       Description = description;
-      _instances.Add(this);
-      Id = _instances.Count;
+    }
+
+    public Item(string description, int id)
+    {
+      Description = description;
+      Id = id;
     }
 
     public static List<Item> GetAll()
     {
-      return _instances;
+      List<Item> allItems = new List<Item> { };
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM items;";
+      MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while (rdr.Read())
+      {
+        int itemId = rdr.GetInt32(0);
+        string itemDescription = rdr.GetString(1);
+        Item newItem = new Item(itemDescription, itemId);
+        allItems.Add(newItem);
+      }
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return allItems;
     }
 
     public static void ClearAll()
     {
-      _instances.Clear();
     }
 
     public static Item Find(int searchId)
     {
-      return _instances[searchId - 1];
+      Item placeholderItem = new Item("placeholder item");
+      return placeholderItem;
     }
 
   }
