1fb289060f4a47f530737745f6a1d89f870b6408
Elly Maimon <eliranmaimon@gmail.com>
2019-07-29 11:24:34 -0700

    Relationships with Entity Framework (Pt. 1)

--- ToDoList/Controllers/CategoriesController.cs
+++ ToDoList/Controllers/CategoriesController.cs
@@ -1,56 +1,72 @@
-// using System.Collections.Generic;
-// using System;
-// using Microsoft.AspNetCore.Mvc;
-// using ToDoList.Models;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
-// namespace ToDoList.Controllers
-// {
-//   public class CategoriesController : Controller
-//   {
-//     [HttpGet("/categories")]
-//     public ActionResult Index()
-//     {
-//       List<Category> allCategories = Category.GetAll();
-//       return View(allCategories);
-//     }
+namespace ToDoList.Controllers
+{
+  public class CategoriesController : Controller
+  {
+    private readonly ToDoListContext _db;
 
-//     [HttpGet("/categories/new")]
-//     public ActionResult New()
-//     {
-//       return View();
-//     }
+    public CategoriesController(ToDoListContext db)
+    {
+      _db = db;
+    }
 
-//     [HttpPost("/categories")]
-//     public ActionResult Create(string categoryName)
-//     {
-//       Category newCategory = new Category(categoryName);
-//       return RedirectToAction("Index");
-//     }
+    public ActionResult Index()
+    {
+      List<Category> model = _db.Categories.ToList();
+      return View(model);
+    }
 
-//     [HttpGet("/categories/{id}")]
-//     public ActionResult Show(int id)
-//     {
-//       Dictionary<string, object> model = new Dictionary<string, object>();
-//       Category selectedCategory = Category.Find(id);
-//       List<Item> categoryItems = selectedCategory.Items;
-//       model.Add("category", selectedCategory);
-//       model.Add("items", categoryItems);
-//       return View(model);
-//     }
+    public ActionResult Create()
+    {
+      return View();
+    }
 
-//     // This one creates new Items within a given Category, not new Categories:
-//     [HttpPost("/categories/{categoryId}/items")]
-//     public ActionResult Create(int categoryId, string itemDescription)
-//     {
-//       Dictionary<string, object> model = new Dictionary<string, object>();
-//       Category foundCategory = Category.Find(categoryId);
-//       Item newItem = new Item(itemDescription);
-//       newItem.Save();
-//       foundCategory.AddItem(newItem);
-//       List<Item> categoryItems = foundCategory.Items;
-//       model.Add("items", categoryItems);
-//       model.Add("category", foundCategory);
-//       return View("Show", model);
-//     }
-//   }
-// }
+    [HttpPost]
+    public ActionResult Create(Category category)
+    {
+      _db.Categories.Add(category);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Category thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
+      return View(thisCategory);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
+      return View(thisCategory);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Category category)
+    {
+      _db.Entry(category).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      var thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
+      return View(thisCategory);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
+      _db.Categories.Remove(thisCategory);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
--- ToDoList/Models/ToDoListContext.cs
+++ ToDoList/Models/ToDoListContext.cs
@@ -4,8 +4,9 @@ namespace ToDoList.Models
 {
   public class ToDoListContext : DbContext
   {
+    public virtual DbSet<Category> Categories { get; set; }
     public DbSet<Item> Items { get; set; }
-
+    
     public ToDoListContext(DbContextOptions options) : base(options) { }
   }
 }
