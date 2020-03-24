c8b21c19b0060485e94ae538eddbd853e0059616
Brooke Kullberg <brookekullberg@gmail.com>
2020-03-24 12:10:41 -0700

    remove 'virtual' from DbSet and add configuration for proxies

--- ToDoList/Models/ToDoListContext.cs
+++ ToDoList/Models/ToDoListContext.cs
@@ -8,5 +8,10 @@ namespace ToDoList.Models
     public DbSet<Item> Items { get; set; }
     
     public ToDoListContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
   }
 }
