f552ce167a5dd2b5dccec91d5a9f9328e64370f7
Brooke Kullberg <brookekullberg@gmail.com>
2020-03-24 12:24:50 -0700

    remove virtual keyword from the DbSet Categories

--- ToDoList/Models/ToDoListContext.cs
+++ ToDoList/Models/ToDoListContext.cs
@@ -4,7 +4,7 @@ namespace ToDoList.Models
 {
   public class ToDoListContext : DbContext
   {
-    public virtual DbSet<Category> Categories { get; set; }
+    public DbSet<Category> Categories { get; set; }
     public DbSet<Item> Items { get; set; }
     
     public ToDoListContext(DbContextOptions options) : base(options) { }
